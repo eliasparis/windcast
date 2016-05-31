61923955f5f92cc760ecf5b49887d64fa52574a7
Elias Pintos <elias.pintos@tecnilogica.com>
2016-05-31 18:10:34 +0200

    Sending data from chrome extension to local database solving troubles with CORS

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -1,8 +1,34 @@
 class VideosController < ApplicationController
 
-  def index
-  	@videos = Video.all
-  	render 'index'
-  end
+	skip_before_action :verify_authenticity_token,	only: [:create]
+	before_filter :cors_preflight_check
+  	after_filter :cors_set_access_control_headers
+
+	def index
+		@videos = Video.all
+		render 'index'
+	end
+
+	def create
+		Video.create(title: params[:title],url: params[:url] ,media_id: params[:video_id]  ,provider: params[:provider_name])
+		head :ok
+	end
+
+	def cors_set_access_control_headers
+		headers['Access-Control-Allow-Origin'] = '*'
+		headers['Access-Control-Allow-Methods'] = 'POST, PUT, DELETE, GET, OPTIONS'
+		headers['Access-Control-Request-Method'] = '*'
+		headers['Access-Control-Allow-Headers'] = 'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+	end
+
+	def cors_preflight_check
+	  if request.method == :options
+		headers['Access-Control-Allow-Origin'] = '*'
+		headers['Access-Control-Allow-Methods'] = 'POST, PUT, DELETE, GET, OPTIONS'
+		headers['Access-Control-Request-Method'] = '*'
+		headers['Access-Control-Allow-Headers'] = 'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+	    render :text => '', :content_type => 'text/plain'
+	  end
+	end
 
 end
