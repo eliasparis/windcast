22e367799690b8725787b0556d3ae188a1c22bc7
Elias Pintos <elias.pintos@tecnilogica.com>
2016-06-02 17:47:02 +0200

    Binding some events to video player

--- app/assets/javascripts/videos.js
+++ app/assets/javascripts/videos.js
@@ -1,5 +1,18 @@
 $(document).on('ready', function(){
 
-	plyr.setup();
+	var player = plyr.setup();
+	
+	var player = document.querySelector('.plyr');
+	player.addEventListener('ended', function(event) {
+		var player = document.querySelector('.plyr').plyr;
+	    player.source({
+	    	type: 'video',
+	    	title: 'aaaa',
+	    	sources: [{
+	    		src: 'bTqVqk7FSmY',
+	    		type: 'youtube'
+	    	}]
+	    });
+	});
 
 });
