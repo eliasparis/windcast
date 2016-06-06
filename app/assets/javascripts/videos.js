d9bc5658848804e00d8183f4c058e0af9b95dfa8
Elias Pintos <elias.pintos@tecnilogica.com>
2016-06-06 18:15:32 +0200

    Next button reactions on UI

--- app/assets/javascripts/videos.js
+++ app/assets/javascripts/videos.js
@@ -1,22 +1,36 @@
-var played = [];
+var mainTitle;
+var	nextTitle;
+var nextThumb;
 
 $(document).on('ready', function(){
 
+	//Plyr variables and events subcriptions
 	plyr.setup();
 	
 	var player = document.querySelector('.plyr');
+	var player_el = document.querySelector('.plyr').plyr;
+	var nextButton = document.getElementById('next_video');
 	
 	player.addEventListener('ended', function(event) {
 
-		var player = document.querySelector('.plyr').plyr;
-		videoController.updateVideo(player);
-
+		videoController.updateVideo(player_el);
 	});
+
+	nextButton.addEventListener('click', function(event) {
+
+		videoController.updateVideo(player_el);
+	});
+
+	//Ui elements
+	mainTitle = $("#main_title");
+	nextTitle = $("#next_title");
+	nextThumb = $("#next_thumb");
 });
 
 var videoController = {
 
 	updateVideo: function(context){
+		
 		if (playlist[0]) {
 
 			var nextVideo = playlist[0];
@@ -28,17 +42,52 @@ var videoController = {
 					type: nextVideo.provider
 				}]
 			}
-			
-			context.source(sourceObj);
-			this.removeJustPlayed();
 
+			context.source(sourceObj);
+			playListManager.updatePLaylist(true);
 		}else{
 
 			return false;
 		};
 	},
+};
+
+var playListManager = {
+
+	updatePLaylist : function(fromNext){
+
+		if (fromNext) {
+			this.addPrevious();
+			this.removeJustPlayed();
+		};
+
+		uiManager.updateUi();
+	},
+	
+	addPrevious: function(){
+		playlist_shadow.unshift(playlist[0]);
+	},
 
 	removeJustPlayed : function(){
 		playlist.shift();
+	},
+};
+
+var uiManager = {
+
+	updateUi: function(){
+		var mainData = playlist_shadow[0];
+		var nextData = playlist[0];
+		this.updateMain(mainData);
+		this.updateNext(nextData);
+	},
+
+	updateMain: function(data){
+		mainTitle.text(data.title);
+	},
+
+	updateNext: function(data){
+		nextTitle.text(data.title);
+		nextThumb.attr('src', data.thumbnail);
 	},
 };
