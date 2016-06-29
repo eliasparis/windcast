0c7ebafdd3e368ff2b40053c8676e834f4605827
Eli <55drk1@gmail.com>
2016-06-29 20:18:54 +0200

    Origin heroku

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,6 +1,8 @@
 class TagsController < ApplicationController
 
 	skip_before_action :verify_authenticity_token,	only: [:create]
+	before_filter :cors_preflight_check, only: [:create]
+  	after_filter :cors_set_access_control_headers, only: [:create]
 
 	def index
 		@tags = Tag.all
@@ -10,6 +12,23 @@ class TagsController < ApplicationController
 	def create
 		tag = Tag.create(name: tag_params[:name])
 		render :text => tag.id
+	end
+
+	def cors_set_access_control_headers
+		headers['Access-Control-Allow-Origin'] = '*'
+		headers['Access-Control-Allow-Methods'] = 'POST, PUT, DELETE, GET, OPTIONS'
+		headers['Access-Control-Request-Method'] = '*'
+		headers['Access-Control-Allow-Headers'] = 'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+	end
+
+	def cors_preflight_check
+		if request.method == :options
+			headers['Access-Control-Allow-Origin'] = '*'
+			headers['Access-Control-Allow-Methods'] = 'POST, PUT, DELETE, GET, OPTIONS'
+			headers['Access-Control-Request-Method'] = '*'
+			headers['Access-Control-Allow-Headers'] = 'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+		    render :text => '', :content_type => 'text/plain'
+		end
 	end
 
 	private
